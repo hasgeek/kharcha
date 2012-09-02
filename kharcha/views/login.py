ad765e43128c779378fb4e3f325f4f8a64d5c24b
Kiran Jonnalagadda <kiran@hasgeek.com>
2012-09-03 05:12:11 +0530

    Sync workspace names and titles on login.

--- kharcha/views/login.py
+++ kharcha/views/login.py
@@ -1,9 +1,10 @@
 # -*- coding: utf-8 -*-
 
-from flask import Response, redirect, flash
+from flask import Response, redirect, flash, g
 from coaster.views import get_next_url
 
 from kharcha import app, lastuser
+from kharcha.models import db, Workspace
 
 
 @app.route('/login')
@@ -22,6 +23,14 @@ def logout():
 @app.route('/login/redirect')
 @lastuser.auth_handler
 def lastuserauth():
+    for org in g.user.organizations_memberof():
+        workspace = Workspace.query.filter_by(userid=org['userid']).first()
+        if workspace:
+            if workspace.name != org['name']:
+                workspace.name = org['name']
+            if workspace.title != org['title']:
+                workspace.title = org['title']
+    db.session.commit()
     return redirect(get_next_url())
 
 
